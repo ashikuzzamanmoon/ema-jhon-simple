3719f49269df7b8cf538a21401e768e5ac4e66da
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-03-28 01:16:43 +0600

    deploy on netlify

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
+
     const { img, name, seller, price, ratings } = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
+
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +19,10 @@ const Product = (props) => {
                 <p>Manufacturar: {seller}</p>
                 <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
