92467217c142859e0868be36532225b075309a61
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-03-28 23:32:19 +0600

    retrive product from stored cart and set it to the setState

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -16,8 +16,24 @@ const Shop = () => {
 
     useEffect( ()=>{
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    },[])
+        const savedCart = [];
+        // step 1: get id of the addedProduct
+        for(const id in storedCart){
+            // step 2: get product from products by using id
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added product to the saved cart
+                savedCart.push(addedProduct);
+            }
+            
+            // console.log('addedProduct', addedProduct);
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    },[products])
 
     const handleAddToCart = (product) =>{
         // cart.push(product);
